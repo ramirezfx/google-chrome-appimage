a572ca9692a1e1b8df6534fa81bee83e9997834f
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2021-09-09 10:12:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ rm -Rf google-chrome-stable_current_amd64.deb
 rm -Rf out
 rm google-chrome-latest.yml
 cp google-chrome-latest.yml.template google-chrome-latest.yml
-sed -i 's/DIST/$DIST/g' google-chrome-latest.yml
+sed -i "s/DIST/$DIST/g" "google-chrome-latest.yml"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ./google-chrome-stable_current_amd64.deb
 ./pkg2appimage.sh google-chrome-latest.yml
